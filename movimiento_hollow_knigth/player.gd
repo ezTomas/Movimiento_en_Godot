e035b4d4e0176f2a38521503442aadb0d631aa6e
Enzo Tomas <tomasenzo03@gmail.com>
2025-04-22 10:46:38 -0300

    Correcion de saltos infijnitos y añadido salto doble

--- movimiento_hollow_knigth/player.gd
+++ movimiento_hollow_knigth/player.gd
@@ -1,17 +1,28 @@
 extends CharacterBody2D
 
 var speed = 200
-var speed_salto = 500
+var speed_salto = 600
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var saltos_maximos = 2
+var saltos_hechos = 0
 
 func _physics_process(delta):
 	var movimientoHorizontal = Input.get_axis("move_left","move_right") 
 	velocity.x = movimientoHorizontal * speed
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y -= speed_salto
+	if is_on_floor():
+		saltos_hechos = 0
+		if Input.is_action_just_pressed("jump"):
+			velocity.y -= speed_salto
+			saltos_hechos += 1
+		
+	else:
+		if Input.is_action_just_pressed("jump") and saltos_hechos < saltos_maximos:
+			velocity.y -= speed_salto
+			saltos_hechos += 1
+	
 	
 	velocity.y += gravity * delta
-	
+	is_on_floor()
 	move_and_slide()
 	
