d4fdd91d08c126fd84051f53e77b0c8bd039dabf
Enzo Tomas <tomasenzo03@gmail.com>
2025-04-23 09:18:01 -0300

    implementación de impulso y salto en la pared

--- movimiento_hollow_knigth/player.gd
+++ movimiento_hollow_knigth/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 200
+var speed = 300
 var speed_salto = 600
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var saltos_maximos = 2
@@ -21,6 +21,16 @@ func _physics_process(delta):
 			velocity.y -= speed_salto
 			saltos_hechos += 1
 	
+	if is_on_wall():
+		saltos_hechos = 0
+		if Input.is_action_just_pressed("jump") and is_on_wall() and saltos_hechos < saltos_maximos:
+			velocity.y -= speed_salto
+			saltos_hechos += 1 
+	
+	if Input.is_action_just_pressed("Dash"):
+		speed = 10000
+		await get_tree().create_timer(0.05).timeout
+		speed = 300
 	
 	velocity.y += gravity * delta
 	is_on_floor()
